49d6f4ae144e9762653c73217e517b0013d40acd
BryanRoth <bryan.roth@hypertherm.com>
2022-09-26 10:08:33 -0400

    Basic MAUI app to add Issues Repro code to.
    Use this as a baseline project to add issues to so that they can be reproduced by developers of libraries.

---
+++ Maui/MauiBugz/MauiBugz/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiBugz;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Maui/MauiBugz/MauiBugz/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiBugz;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
